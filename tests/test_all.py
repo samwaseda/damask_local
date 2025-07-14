9f577badf3a9c6e25ce07f570efdb40e0de7eb03
Sam Waseda <waseda@mpie.de>
2025-07-14 17:35:44 +0200

    Add simple test

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,10 +1,13 @@
 import unittest
 
+from damask_local import main as dsk
+
 
 class TestFull(unittest.TestCase):
 
-    def test_run(self):
-        pass
+    def test_get_lattice_structure(self):
+        self.assertEqual(dsk._get_lattice_structure(chemical_symbol="Al"), "cF")
+        self.assertEqual(dsk._get_lattice_structure(chemical_symbol="Aluminum"), "cF")
 
 
 if __name__ == '__main__':
